13dc6e92c7799b90ed14999ce525cd19b3d7a8a9
bn-tw2020 <skaxodn97@gmail.com>
2022-03-23 11:53:33 +0900

    feat: 다운로드 이미지 리사이클러 뷰에 적용하기

--- app/src/main/java/com/example/kotlinphotos/photos/PhotoActivity.kt
+++ app/src/main/java/com/example/kotlinphotos/photos/PhotoActivity.kt
@@ -1,17 +1,14 @@
 package com.example.kotlinphotos.photos
 
 import android.content.Intent
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageButton
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinphotos.R
 
 class PhotoActivity : AppCompatActivity() {
-    val TAG = "PhotoActivity"
     private lateinit var plusImageButton: ImageButton
     private lateinit var recyclerView: RecyclerView
 
@@ -22,9 +19,8 @@ class PhotoActivity : AppCompatActivity() {
         plusImageButton = findViewById<ImageButton>(R.id.image_button_plus)
         recyclerView = findViewById<RecyclerView>(R.id.recyclerview_images)
         val photos = mutableListOf<Photo>()
-        //val example = BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_add_24)
 
-        recyclerView.adapter = PhotosAdaptor(photos, PhotosDiffCallback())
+        recyclerView.adapter = PhotosAdapter(photos, PhotosDiffCallback())
         recyclerView.layoutManager = GridLayoutManager(this, 3)
 
         setPlusImageButtonListener()
